0d3e9fcf09e1db71879e999b16d2a00b603956ad
Ben Dixon <ben@hillsbede.co.uk>
2013-06-20 20:56:11 +0100

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "look_and_feel-tlq"
+maintainer        "Ben Dixon"
+maintainer_email  "ben@hillsbede.co.uk"
+description       "Installs tools to make the server nicer to work on, quite opinionated"
+version           "0.0.1"
+
+recipe "look_and_feel-tlq", "Installs all look and feel options"
+
+supports "ubuntu"
---
+++ recipes/default.rb
@@ -0,0 +1,40 @@
+# htop is a prettier (but more resource intensive) alternative
+# to top.
+package 'htop'
+package 'vim'
+package 'unzip'
+
+# Add a banner to ssh login if we're in the production environment
+# (from Rich)
+
+if in_production_env?
+
+  sshd_config = '/etc/ssh/sshd_config'
+
+  seds = []
+  echos = []
+
+  banner_path = '/etc/ssh_banner'
+
+  seds << 's/^Banner/#Banner/g'
+  echos << "Banner #{banner_path}"
+
+  template banner_path do
+    owner 'root'
+    group 'root'
+    mode '0644'
+    source 'production_ssh_banner.erb'
+  end
+
+  bash 'Adding visual flags for production environment' do
+    user 'root'
+    code <<-EOC
+      #{seds.map { |rx| "sed -i '#{rx}' #{sshd_config}" }.join("\n")}
+      #{echos.map { |e| %Q{echo "#{e}" >> #{sshd_config}} }.join("\n")}
+    EOC
+  end
+
+  service 'ssh' do
+    action :restart
+  end
+end
